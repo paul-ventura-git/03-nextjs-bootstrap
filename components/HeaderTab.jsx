16b7cca858482175f5bc9f4803894617f202d3ef
paul-ventura-git <j.paul.ventura@gmail.com>
2025-01-05 18:12:51 -0500

    HeaderTab

--- components/HeaderTab.jsx
+++ components/HeaderTab.jsx
@@ -2,11 +2,27 @@ import React from 'react'
 
 export default function HeaderTab() {
   return (
-    <nav className="nav container">
-        <a className="nav-link link-dark active" aria-current="page" href="#">Falabella</a>
-        <a className="nav-link link-dark" href="#">Sodimac</a>
-        <a className="nav-link link-dark" href="#">Tottus</a>
-        <a className="nav-link link-dark disabled" href="#" tabIndex="-1" aria-disabled="true">Linio</a>
-    </nav>
+    <div className='container'>
+      <ul className="nav nav-tabs" id="myTab" role="tablist">
+        <li className="nav-item" role="presentation">
+          <button className="nav-link active" id="falabella-tab" data-bs-toggle="tab" data-bs-target="#falabella" type="button" role="tab" aria-controls="falabella" aria-selected="true">Falabella</button>
+        </li>
+        <li className="nav-item" role="presentation">
+          <button className="nav-link" id="sodimac-tab" data-bs-toggle="tab" data-bs-target="#sodimac" type="button" role="tab" aria-controls="sodimac" aria-selected="false">Sodimac</button>
+        </li>
+        <li className="nav-item" role="presentation">
+          <button className="nav-link" id="tottus-tab" data-bs-toggle="tab" data-bs-target="#tottus" type="button" role="tab" aria-controls="tottus" aria-selected="false">Tottus</button>
+        </li>
+        <li className="nav-item" role="presentation">
+          <button className="nav-link" id="linio-tab" data-bs-toggle="tab" data-bs-target="#linio" type="button" role="tab" aria-controls="linio" aria-selected="false">Linio</button>
+        </li>
+      </ul>
+      <div className="tab-content" id="myTabContent">
+        <div className="tab-pane fade show active" id="falabella" role="tabpanel" aria-labelledby="home-tab">Content 1</div>
+        <div className="tab-pane fade" id="sodimac" role="tabpanel" aria-labelledby="profile-tab">Content 2</div>
+        <div className="tab-pane fade" id="tottus" role="tabpanel" aria-labelledby="contact-tab">Content 3</div>
+        <div className="tab-pane fade" id="linio" role="tabpanel" aria-labelledby="contact-tab">Content 4</div>
+      </div>
+    </div>
   )
 }
