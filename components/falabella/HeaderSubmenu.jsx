79b3260110d9379b5ac2938dc667d9d9aff04adf
paul-ventura-git <j.paul.ventura@gmail.com>
2025-01-06 14:45:51 -0500

    Modal

--- components/falabella/HeaderSubmenu.jsx
+++ components/falabella/HeaderSubmenu.jsx
@@ -10,7 +10,7 @@ export default function HeaderSubmenu() {
         <div className="collapse navbar-collapse" id="navbarText">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#"><FaLocationDot /> Ingresa tu ubicación</a>
+              <a className="nav-link active" aria-current="page" href="#" data-bs-toggle="modal" data-bs-target="#exampleModal"><FaLocationDot /> Ingresa tu ubicación</a>
             </li>
           </ul>
           <div className="dropdown">
@@ -42,6 +42,45 @@ export default function HeaderSubmenu() {
               <li><a className="dropdown-item" href="#">Another action</a></li>
               <li><a className="dropdown-item" href="#">Something else here</a></li>
             </ul>
+          </div>
+        </div>
+        {/** Modal */}
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Ingresa tu ubicación</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div class="modal-body">
+                <p>Ingresa tu ubicación y te mostraremos los productos disponibles con tiempos y costos de entrega precisos.</p>
+                <select class="form-select" aria-label="Default select example">
+                  <option defaultValue={''}>Ingresa un departamento</option>
+                  <option value="1">One</option>
+                  <option value="2">Two</option>
+                  <option value="3">Three</option>
+                </select>
+                <br />
+                <select class="form-select" aria-label="Default select example">
+                  <option defaultValue={''}>Ingresa una provincia</option>
+                  <option value="1">One</option>
+                  <option value="2">Two</option>
+                  <option value="3">Three</option>
+                </select>
+                <br />
+                <select class="form-select" aria-label="Default select example">
+                  <option defaultValue={''}>Ingresa un distrito</option>
+                  <option value="1">One</option>
+                  <option value="2">Two</option>
+                  <option value="3">Three</option>
+                </select>
+                <br />
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
